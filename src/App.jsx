b177098d3a8f3f50b9f3ceae968164c1db6a86bd
Vawndyu <vonnedewsalig@gmail.com>
2025-02-26 16:54:09 +0800

    modified:   src/App.jsx 	new file:   src/assets/background.jpg 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import wrongSound from "./assets/sounds/wrong.mp3";      // Add a wrong sound fi
 import bgMusic from "./assets/sounds/background.mp3";  // Background music
 
 const images = [
-  {src: "src/assets/img/apple.png", answer: "apple", hint: "It's a fruit"},
-  {src: "src/assets/img/ball.png", answer: "ball", hint: "A round object which is used in sports and games"},
-  {src: "src/assets/img/balloon.png", answer: "balloon", hint: "is a bag that is usually filled with gas"},
-  {src: "src/assets/img/banana.jpg", answer: "banana", hint: "It's a fruit."},
-  {src: "src/assets/img/bee.png", answer: "bee", hint: "Collect pollen and nectar from flowers to make honey."},
-  {src: "src/assets/img/cat.png", answer: "cat", hint: "It's a common pet."},
-  {src: "src/assets/img/cherry.jpg", answer: "cherry", hint: "It's a fruit."},
-  {src: "src/assets/img/durian.jpg", answer: "durian", hint: "a large, spiky tropical fruit indigenous to Southeast Asia."},
-  {src: "src/assets/img/pencil.png", answer: "pencil", hint: "Use for writing."}
+  {src: "img/apple.png", answer: "apple", hint: "It's a fruit"},
+  {src: "img/ball.png", answer: "ball", hint: "A round object which is used in sports and games"},
+  {src: "img/balloon.png", answer: "balloon", hint: "is a bag that is usually filled with gas"},
+  {src: "img/banana.jpg", answer: "banana", hint: "It's a fruit."},
+  {src: "img/bee.png", answer: "bee", hint: "Collect pollen and nectar from flowers to make honey."},
+  {src: "img/cat.png", answer: "cat", hint: "It's a common pet."},
+  {src: "img/cherry.jpg", answer: "cherry", hint: "It's a fruit."},
+  {src: "img/durian.jpg", answer: "durian", hint: "a large, spiky tropical fruit indigenous to Southeast Asia."},
+  {src: "img/pencil.png", answer: "pencil", hint: "Use for writing."}
 ];
 
 function App() {
